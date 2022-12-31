eb42360df920ce39239a24ba581bbd46e090f425
Josef Friedrich <josef@friedrich.rocks>
2022-12-31 15:19:32 +0100

    Add first test code

---
+++ luatex/node.lua
@@ -0,0 +1,13 @@
+---@meta
+
+local node = {}
+
+---Test mode to see if this type definition library gets picked up.
+---Not really a function.
+---
+---@param lol string
+---@return string
+function node.lol(lol)
+end
+
+return node
