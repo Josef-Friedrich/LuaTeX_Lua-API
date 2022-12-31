3acf7d3f5b35dbe5c4516bd525fe88cbb2ca2ea6
Josef Friedrich <josef@friedrich.rocks>
2022-12-31 16:07:23 +0100

    Add first functions of the node library

--- luatex/node.lua
+++ luatex/node.lua
@@ -1,13 +1,62 @@
 ---@meta
 
-local node = {}
+--- https://github.com/TeX-Live/luatex/blob/trunk/manual/luatex-nodes.tex
 
----Test mode to see if this type definition library gets picked up.
----Not really a function.
+---The node library contains functions that facilitate dealing with (lists of) nodes and their values.
+---They allow you to create, alter, copy, delete, and insert LuaTEX node objects, the core objects
+---within the typesetter.
 ---
----@param lol string
----@return string
-function node.lol(lol)
-end
+---LuaTEX nodes are represented in Lua as userdata with the metadata type `luatex.node.` The
+---various parts within a node can be accessed using named fields.
+---
+---Each node has at least the three fields next, id, and subtype:
+---
+---* The next field returns the userdata object for the next node in a linked list of nodes, or nil,
+---  if there is no next node.
+---* The id indicates TEX’s ‘node type’. The field id has a numeric value for efficiency reasons,
+---  but some of the library functions also accept a string value instead of id.
+---* The subtype is another number. It often gives further information about a node of a particular
+---  id, but it is most important when dealing with ‘whatsits’, because they are differentiated
+---  solely based on their subtype.
+---
+---The other available fields depend on the id (and for ‘whatsits’, the subtype) of the node.
+---
+---Support for unset (alignment) nodes is partial: they can be queried and modified from Lua code,
+---but not created.
+---
+---Nodes can be compared to each other, but: you are actually comparing indices into the node
+---memory. This means that equality tests can only be trusted under very limited conditions. It will
+---not work correctly in any situation where one of the two nodes has been freed and/or reallocated:
+---in that case, there will be false positives.
+---
+---At the moment, memory management of nodes should still be done explicitly by the user. Nodes
+---are not ‘seen’ by the Lua garbage collector, so you have to call the node freeing functions yourself
+---when you are no longer in need of a node (list). Nodes form linked lists without reference
+---counting, so you have to be careful that when control returns back to LuaTEX itself, you have
+---not deleted nodes that are still referenced from a next pointer elsewhere, and that you did not
+---create nodes that are referenced more than once. Normally the setters and getters handle this
+---for you.
+---
+---There are statistics available with regards to the allocated node memory, which can be handy
+---for tracing.
+node = {}
 
-return node
+---This function returns a number (the internal index of the node) if the argument is a userdata
+---object of type <node> and false when no node is passed.
+---
+---@param item any
+---
+---@return boolean|integer t
+function node.is_node(item) end
+
+---This function returns an array that maps node id numbers to node type strings, providing an
+---overview of the possible top-level id types.
+---
+---@return table
+function node.types() end
+
+---TEX’s ‘whatsits’ all have the same id. The various subtypes are defined by their subtype fields.
+---The function is much like types, except that it provides an array of subtype mappings.
+---
+---@return table
+function node.whatsits() end
