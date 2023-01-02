218842fa4cbbae22d4a2e9e27121713e0cdfdad8
Josef Friedrich <josef@friedrich.rocks>
2023-01-02 08:16:02 +0100

    Improve documentation of the token library

--- src/luatex/token.lua
+++ src/luatex/token.lua
@@ -1,7 +1,5 @@
 ---@meta
 
----https://github.com/TeX-Live/luatex/blob/3f14129c06359e1a06dd2f305c8334a2964149d3/manual/luatex-tex.tex#L2091
-
 ---The token library provides means to intercept the input and deal with it at the
 ---Lua level. The library provides a basic scanner infrastructure that can be used
 ---to write macros that accept a wide range of arguments. This interface is on
@@ -10,10 +8,25 @@
 ---they can benefit from this as the main principle behind LuaTeX is to provide a
 ---minimal set of tools and no solutions. The scanner functions are probably the
 ---most intriguing.
+------
+---Source: [luatex-tex.tex#L2091-L2098](https://github.com/TeX-Live/luatex/blob/3f14129c06359e1a06dd2f305c8334a2964149d3/manual/luatex-tex.tex#L2091-L2098)
+---
 token = {}
 
----https://github.com/TeX-Live/luatex/blob/3f14129c06359e1a06dd2f305c8334a2964149d3/manual/luatex-tex.tex#L2285
-
+---
+---A token object has the properties of the `relax`
+---primitive. The possible properties of tokens are:
+---
+---Alternatively you can use a getter `get_<fieldname>` to access a property
+---of a token.
+---
+---The numbers that represent a catcode are the same as in *TeX* itself, so using
+---this information assumes that you know a bit about *TeX*'s internals. The other
+---numbers and names are used consistently but are not frozen. So, when you use them
+---for comparing you can best query a known primitive or character first to see the
+---values.
+------
+---Source: [luatex-tex.tex#L2285-L2310](https://github.com/TeX-Live/luatex/blob/3f14129c06359e1a06dd2f305c8334a2964149d3/manual/luatex-tex.tex#L2285-L2310)
 ---
 ---@class Token
 ---@field command integer # a number representing the internal command number
@@ -30,21 +43,24 @@ token = {}
 ---
 ---The scanners look for a sequence. When you want to pick up one token from the
 ---input you use `get_next`. This creates a token with the (low level)
----properties.
+---properties. This token is just the next one.
+------
+---Source: [luatex-tex.tex#L2237-L2239](https://github.com/TeX-Live/luatex/blob/3f14129c06359e1a06dd2f305c8334a2964149d3/manual/luatex-tex.tex#L2237-L2239)
 ---
 ---@return Token
 function token.get_next() end
-
----https://github.com/TeX-Live/luatex/blob/3f14129c06359e1a06dd2f305c8334a2964149d3/manual/luatex-tex.tex#L2422
 
 ---
 ---There is a (for now) experimental putter.
 ---
 ---@param ... Token
+------
+---Source: [luatex-tex.tex#L2422-L2433](https://github.com/TeX-Live/luatex/blob/3f14129c06359e1a06dd2f305c8334a2964149d3/manual/luatex-tex.tex#L2422-L2433)
+---
 function token.put_next(...) end
 
----https://github.com/TeX-Live/luatex/blob/3f14129c06359e1a06dd2f305c8334a2964149d3/manual/luatex-tex.tex#L2368
-
+------
+---Source: [luatex-tex.tex#L2368-L2382](https://github.com/TeX-Live/luatex/blob/3f14129c06359e1a06dd2f305c8334a2964149d3/manual/luatex-tex.tex#L2368-L2382)
 ---
 ---@param csname string
 ---@param content? string
@@ -54,6 +70,8 @@ function token.set_macro(csname, content, global) end
 ---
 ---The `set_macro` function can get upto 4 arguments.
 ---You can pass a catcodetable identifier as first argument.
+------
+---Source: [luatex-tex.tex#L2368-L2382](https://github.com/TeX-Live/luatex/blob/3f14129c06359e1a06dd2f305c8334a2964149d3/manual/luatex-tex.tex#L2368-L2382)
 ---
 ---@param catcodetable any
 ---@param csname string
