e7d95ee9b4a656895a6dd1f084b248a5901d15db
Josef Friedrich <josef@friedrich.rocks>
2023-01-05 14:45:06 +0100

    Document modules

---
+++ library/modules.lua
@@ -0,0 +1,11 @@
+---@meta
+
+---@type { [string]: Module }
+modules = {}
+
+---@class Module
+---@field version string # for example "v0.42.0"
+---@field comment string # for example "companion to luat-lib.mkiv",
+---@field author string # for example "Donald Knuth",
+---@field copyright string # for example "TeX Development Team",
+---@field license string # for example "GPL v2"
