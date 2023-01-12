fb9557a85f4d649d8715dfbe22967038acd4ddd3
Josef Friedrich <josef@friedrich.rocks>
2023-01-12 22:22:20 +0100

    Document kpse

--- library/kpse.lua
+++ library/kpse.lua
@@ -1,6 +1,64 @@
 ---@meta
 
+---
+---This library provides two separate, but nearly identical interfaces to the
+---`kpathsea` file search functionality: there is a “normal” procedural
+---interface that shares its kpathsea instance with *LuaTeX* itself, and an object
+---oriented interface that is completely on its own.
 kpse = {}
+
+---
+---The way the library looks up variables is driven by the `texmf.cmf` file
+---where the currently set program name acts as filter. You can check what file is
+---used by with `default_texmfcnf`.
+---
+---Before the search library can be used at all, its database has to be initialized.
+---There are three possibilities, two of which belong to the procedural interface.
+---
+---First, when *LuaTeX* is used to typeset documents, this initialization happens
+---automatically and the `kpathsea` executable and program names are set to `luatex` (that is, unless explicitly prohibited by the user's startup script.
+---
+---Second, in *TeX*LUA\ mode, the initialization has to be done explicitly via the
+---`kpse.set_program_name` function, which sets the `kpathsea` executable
+---(and optionally program) name.
+---
+---
+---The second argument controls the use of the “dotted” values in the `texmf.cnf` configuration file, and defaults to the first argument.
+---
+---Third, if you prefer the object oriented interface, you have to call a different
+---function. It has the same arguments, but it returns a userdata variable.
+---
+---@param name string
+---@param progname? string
+function kpse.set_program_name(name, progname) end
+
+---
+---Third, if you prefer the object oriented interface, you have to call a different
+---function. It has the same arguments, but it returns a userdata variable.
+---
+---@param name string
+---@param progname? string
+---
+---@return userdata
+function kpse.new(name, progname) end
+
+---
+---Register used files.
+---
+---Because callbacks can load
+---files themselves you might need these helpers (if you use recording at all).
+---
+---@param name string
+function kpse.record_input_file(name) end
+
+---
+---Register used files.
+---
+---Because callbacks can load
+---files themselves you might need these helpers (if you use recording at all).
+---
+---@param name string
+function kpse.record_output_file(name) end
 
 ------------------------------------------------------------------------
 ---Undocumented functions listed in alphabetical order
@@ -61,31 +119,7 @@ function kpse.lookup() end
 ---Warning! Undocumented code!<p>
 ---TODO: Please contribute
 ---https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
-function kpse.new() end
-
----
----Warning! Undocumented code!<p>
----TODO: Please contribute
----https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
 function kpse.readable_file() end
-
----
----Warning! Undocumented code!<p>
----TODO: Please contribute
----https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
-function kpse.record_input_file() end
-
----
----Warning! Undocumented code!<p>
----TODO: Please contribute
----https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
-function kpse.record_output_file() end
-
----
----Warning! Undocumented code!<p>
----TODO: Please contribute
----https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
-function kpse.set_program_name() end
 
 ---
 ---Warning! Undocumented code!<p>
