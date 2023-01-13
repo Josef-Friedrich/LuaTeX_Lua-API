4bf76ed68d3d3d56b47b892afbbeff9ceeeb427d
Josef Friedrich <josef@friedrich.rocks>
2023-01-13 18:21:15 +0100

    Add a script to search in some text files

---
+++ search.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+# --context=num
+
+#     Print num lines of leading and trailing output context.
+
+grep \
+  --context=3 \
+  --group-separator=----------------------------------------- \
+  --recursive \
+  --line-number \
+  --colour \
+  "$*" "$HOME/repos/github/TeX-mailing-lists" /usr/local/texlive
