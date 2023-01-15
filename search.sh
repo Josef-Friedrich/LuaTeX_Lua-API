152ccd5feb25cb8cf387be11a21d74b0ded05644
Josef Friedrich <josef@friedrich.rocks>
2023-01-15 10:04:49 +0100

    Update the search.sh script

--- search.sh
+++ search.sh
@@ -1,13 +1,14 @@
 #! /bin/sh
 
 # --context=num
-
 #     Print num lines of leading and trailing output context.
-
 grep \
   --context=3 \
   --group-separator=----------------------------------------- \
   --recursive \
   --line-number \
   --colour \
-  "$*" "$HOME/repos/github/TeX-mailing-lists" /usr/local/texlive .
+  "$*" \
+  "$HOME/repos/tex/mailing_lists" \
+  /usr/local/texlive \
+  .
