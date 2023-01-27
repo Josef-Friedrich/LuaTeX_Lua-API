ed8d3f20790e3930988bca9aa5de9f1dbb22698c
Josef Friedrich <josef@friedrich.rocks>
2023-01-27 20:00:12 +0100

    Document lualibs/dir

--- library/lualibs/dir.lua
+++ library/lualibs/dir.lua
@@ -1,88 +1,75 @@
 ---@meta
-
 dir = {}
 
-------------------------------------------------------------------------
----Undocumented functions listed in alphabetical order
 ---
----Document them by sliding them up and place them in the order of the
----official documentation
-------------------------------------------------------------------------
-
----
----Warning! Undocumented code!<p>
----TODO: Please contribute
----https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
-function dir.collectpattern() end
-
----
----Warning! Undocumented code!<p>
----TODO: Please contribute
----https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
+---* Corresponding source code: [lualibs-dir.lua#L74-L76](https://github.com/latex3/lualibs/blob/116ffb3eddeb32663879200b91bc3177d74ee1ff/lualibs-dir.lua#L74-L76)
 function dir.current() end
 
 ---
----Warning! Undocumented code!<p>
----TODO: Please contribute
----https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
-function dir.expandname() end
+---* Corresponding source code: [lualibs-dir.lua#L169-L182](https://github.com/latex3/lualibs/blob/116ffb3eddeb32663879200b91bc3177d74ee1ff/lualibs-dir.lua#L169-L182)
+---
+function dir.globpattern(path, patt, recurse, method) end
 
 ---
----Warning! Undocumented code!<p>
----TODO: Please contribute
----https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
-function dir.found() end
+---* Corresponding source code: [lualibs-dir.lua#L188-L221](https://github.com/latex3/lualibs/blob/116ffb3eddeb32663879200b91bc3177d74ee1ff/lualibs-dir.lua#L188-L221)
+function dir.collectpattern(path, patt, recurse, result) end
 
 ---
----Warning! Undocumented code!<p>
----TODO: Please contribute
----https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
-function dir.glob() end
+---__Example:__
+---
+---```lua
+---local list = dir.glob("e:/**")
+---local list = dir.glob("t:/sources/**")
+---local list = dir.glob("**/*.tif")
+---```
+---
+---* Corresponding source code: [lualibs-dir.lua#L259-L302](https://github.com/latex3/lualibs/blob/116ffb3eddeb32663879200b91bc3177d74ee1ff/lualibs-dir.lua#L259-L302)
+function dir.glob(str, t) end
 
 ---
----Warning! Undocumented code!<p>
----TODO: Please contribute
----https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
-function dir.globdirs() end
+---* Corresponding source code: [lualibs-dir.lua#L321-L348](https://github.com/latex3/lualibs/blob/116ffb3eddeb32663879200b91bc3177d74ee1ff/lualibs-dir.lua#L321-L348)
+function dir.globfiles(path, recurse, func, files) end
 
 ---
----Warning! Undocumented code!<p>
----TODO: Please contribute
----https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
-function dir.globfiles() end
+---* Corresponding source code: [lualibs-dir.lua#L352-L378](https://github.com/latex3/lualibs/blob/116ffb3eddeb32663879200b91bc3177d74ee1ff/lualibs-dir.lua#L352-L378)
+function dir.globdirs(path, recurse, func, files) end
 
 ---
----Warning! Undocumented code!<p>
----TODO: Please contribute
----https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
-function dir.globpattern() end
+---* Corresponding source code: [lualibs-dir.lua#L391-L393](https://github.com/latex3/lualibs/blob/116ffb3eddeb32663879200b91bc3177d74ee1ff/lualibs-dir.lua#L391-L393)
+---
+---@param pattern string
+---
+---@return string
+function dir.ls(pattern) end
 
 ---
----Warning! Undocumented code!<p>
----TODO: Please contribute
----https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
-function dir.ls() end
+---* Corresponding source code: [lualibs-dir.lua#L402-L536](https://github.com/latex3/lualibs/blob/116ffb3eddeb32663879200b91bc3177d74ee1ff/lualibs-dir.lua#L402-L536)
+---
+---@param ... string
+function dir.makedirs(...) end
 
 ---
----Warning! Undocumented code!<p>
----TODO: Please contribute
----https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
-function dir.makedirs() end
+---* Corresponding source code: [lualibs-dir.lua#L402-L536](https://github.com/latex3/lualibs/blob/116ffb3eddeb32663879200b91bc3177d74ee1ff/lualibs-dir.lua#L402-L536)
+---
+---@param ... string
+function dir.mkdirs(...) end
 
 ---
----Warning! Undocumented code!<p>
----TODO: Please contribute
----https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
-function dir.mkdirs() end
+---* Corresponding source code: [lualibs-dir.lua#L553-L597](https://github.com/latex3/lualibs/blob/116ffb3eddeb32663879200b91bc3177d74ee1ff/lualibs-dir.lua#L553-L597)
+---
+---@param str string
+function dir.expandname(str) end
 
 ---
----Warning! Undocumented code!<p>
----TODO: Please contribute
----https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
+---* Corresponding source code: [lualibs-dir.lua#L605-L613](https://github.com/latex3/lualibs/blob/116ffb3eddeb32663879200b91bc3177d74ee1ff/lualibs-dir.lua#L605-L613)
+---
+function dir.push() end
+
+---
+---* Corresponding source code: [lualibs-dir.lua#L615-L621](https://github.com/latex3/lualibs/blob/116ffb3eddeb32663879200b91bc3177d74ee1ff/lualibs-dir.lua#L615-L621)
 function dir.pop() end
 
 ---
----Warning! Undocumented code!<p>
----TODO: Please contribute
----https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
-function dir.push() end
+---* Corresponding source code: [lualibs-dir.lua#L623-L640](https://github.com/latex3/lualibs/blob/116ffb3eddeb32663879200b91bc3177d74ee1ff/lualibs-dir.lua#L623-L640)
+---
+function dir.found() end
