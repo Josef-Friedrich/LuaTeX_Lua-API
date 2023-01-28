a5b64611d2113bb1c7d0714724f1a0ba1a1ecc15
Josef Friedrich <josef@friedrich.rocks>
2023-01-28 20:04:52 +0100

    Document the lualibs.url lib

--- library/lualibs/os.lua
+++ library/lualibs/os.lua
@@ -4,11 +4,13 @@
 
 os = {}
 
+function os.setenv(k, v) end
+function os.getenv(k) end
+
 function os.converttime(t, default) end
 function os.date(fmt) end
 function os.exit(c) end
 function os.fulltime(t, default) end
-function os.getenv(k) end
 function os.gettimeofday() end
 function os.isleapyear(year) end
 function os.launch(str) end
@@ -16,7 +18,6 @@ function os.localtime(t, default) end
 function os.nofdays(year, month, day) end
 function os.pipeto(command) end
 function os.resultof(command) end
-function os.setenv(k, v) end
 function os.setexitcode(code) end
 function os.timezone(difference) end
 function os.validdate(year, month, day) end
--- library/lualibs/url.lua
+++ library/lualibs/url.lua
@@ -1,8 +1,36 @@
 ---@meta
 
+---This is a specialized library. You can split an url into its components. An URL is constructed like this:
 ---https://github.com/latex3/lualibs/blob/main/lualibs-url.lua
 
 url = {}
+
+---Example `foo://example.com:2010/alpha/beta?gamma=delta#epsilon`
+---@class Url
+---@field scheme string # for example `foo`.
+---@field authority string # for example `example.com:2010`.
+---@field filename string # for example `example.com:2010/alpha/beta`.
+---@field path string # for example `alpha/beta`.
+---@field port string # for example `2010`.
+---@field query string # for example `gamma=delta`.
+---@field queries table # for example `{["gamma"]="delta"}`.
+---@field fragment string # for example `epsilon`.
+---@field original string # for example `foo://example.com:2010/alpha/beta?gamma=delta#epsilon"`.
+---@field noscheme boolean # for example `false`.
+
+---A string is split into a hash table with these keys using the following function:
+---The hash variant is more tolerant than the split. In the hash there is also a key `original` that holds
+---the original URL and and the boolean noscheme indicates if there is a scheme at all.
+---@param str string
+---
+---@return Url
+function url.hashed(str) end
+
+---
+---@param str string
+---
+---@return string[]
+function url.split(str) end
 
 ------------------------------------------------------------------------
 ---Undocumented functions listed in alphabetical order
@@ -57,12 +85,6 @@ function url.filename(filename) end
 ---Warning! Undocumented code!<p>
 ---TODO: Please contribute
 ---https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
-function url.hashed(str) end
-
----
----Warning! Undocumented code!<p>
----TODO: Please contribute
----https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
 function url.hasscheme(str) end
 
 ---
@@ -70,12 +92,6 @@ function url.hasscheme(str) end
 ---TODO: Please contribute
 ---https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
 function url.query() end
-
----
----Warning! Undocumented code!<p>
----TODO: Please contribute
----https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
-function url.split(str) end
 
 ---
 ---Warning! Undocumented code!<p>
