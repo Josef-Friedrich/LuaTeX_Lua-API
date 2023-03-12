136811c35165feacb23a5d39329636a9b90f7755
Josef Friedrich <josef@friedrich.rocks>
2023-03-12 17:27:03 +0100

    Add some tests

--- examples/unicode/all.lua
+++ examples/unicode/all.lua
@@ -21,25 +21,20 @@ it('unicode.xxx.char', function()
 end)
 
 it('unicode.xxx.dump', function()
-  local f = function() return 'Test' end
-  assert.is.equal(type(unicode.ascii.dump(f)), 'string')
-  assert.is.equal(type(unicode.latin1.dump(f)), 'string')
-  assert.is.equal(type(unicode.grapheme.dump(f)), 'string')
-  assert.is.equal(type(unicode.utf8.dump(f)), 'string')
+    local f = function() return 'Test' end
+    assert.is.equal(type(unicode.ascii.dump(f)), 'string')
+    assert.is.equal(type(unicode.latin1.dump(f)), 'string')
+    assert.is.equal(type(unicode.grapheme.dump(f)), 'string')
+    assert.is.equal(type(unicode.utf8.dump(f)), 'string')
 end)
 
--- ---
--- ---Looks for the first match of pattern in the string.
--- ---
--- ---@param s string
--- ---@param pattern string
--- ---@param init? integer
--- ---@param plain? boolean
--- ---
--- ---@return integer start
--- ---@return integer end
--- ---@return any ... captured
--- function unicode.utf8.find(s, pattern, init, plain) end
+it('unicode.xxx.find', function()
+  print(unicode.ascii.find('Test', 'e', 1, true))
+  assert.is.equal(unicode.ascii.find('Test', 'e'), 'Test')
+  assert.is.equal(unicode.latin1.char(84, 101, 115, 116), 'Test')
+  assert.is.equal(unicode.grapheme.char(84, 101, 115, 116), 'Test')
+  assert.is.equal(unicode.utf8.char(84, 101, 115, 116), 'Test')
+end)
 
 -- ---
 -- ---Returns a formatted version of its variable number of arguments following the description given in its first argument.
@@ -50,28 +45,34 @@ end)
 -- ---@return string
 -- function unicode.utf8.format(s, ...) end
 
--- function unicode.utf8.gfind() end
+it('unicode.xxx.gfind', function()
+    assert.has_error(function() unicode.ascii.gfind('test') end,
+                     "'string.gfind' was renamed to 'string.gmatch'")
 
--- ---
--- ---Returns an iterator function that, each time it is called, returns the next captures from `pattern` over the string `s`.
--- ---
--- ---@param s string
--- ---@param pattern string
--- ---
--- ---@return fun():string, ...
--- function unicode.utf8.gmatch(s, pattern) end
+    assert.has_error(function() unicode.latin1.gfind('test') end,
+                     "'string.gfind' was renamed to 'string.gmatch'")
 
--- ---
--- ---Returns a copy of `s` in which all (or the first `n`, if given) occurrences of the pattern have been replaced by a replacement string specified by `repl`.
--- ---
--- ---@param s string
--- ---@param pattern string
--- ---@param repl string|number|table|function
--- ---@param n? integer
--- ---
--- ---@return string
--- ---@return integer count
--- function unicode.utf8.gsub(s, pattern, repl, n) end
+    assert.has_error(function() unicode.grapheme.gfind('test') end,
+                     "'string.gfind' was renamed to 'string.gmatch'")
+
+    assert.has_error(function() unicode.utf8.gfind('test') end,
+                     "'string.gfind' was renamed to 'string.gmatch'")
+
+end)
+
+it('unicode.xxx.gmatch', function()
+    assert.is.equal(type(unicode.ascii.gmatch('test', 't')), 'function')
+    assert.is.equal(type(unicode.latin1.gmatch('test', 't')), 'function')
+    assert.is.equal(type(unicode.grapheme.gmatch('test', 't')), 'function')
+    assert.is.equal(type(unicode.utf8.gmatch('test', 't')), 'function')
+end)
+
+it('unicode.xxx.gsub', function()
+  assert.is.equal(unicode.ascii.gsub('test', 't', 'x', 1), 'xest')
+  assert.is.equal(unicode.latin1.gsub('test', 't', 'x', 1), 'xest')
+  assert.is.equal(unicode.grapheme.gsub('test', 't', 'x', 1), 'xest')
+  assert.is.equal(unicode.utf8.gsub('test', 't', 'x', 1), 'xest')
+end)
 
 -- ---
 -- ---Returns its length.
@@ -89,39 +90,20 @@ end)
 -- ---@return string
 -- function unicode.utf8.lower(s) end
 
--- ---
--- ---Looks for the first match of pattern in the string.
--- ---
--- ---@param s string
--- ---@param pattern string
--- ---@param init? integer
--- ---
--- ---@return any ...
--- function unicode.utf8.match(s, pattern, init) end
-
 it('unicode.xxx.match', function()
-  assert.is.equal(unicode.ascii.match('Test', 'e', 3), nil)
+    assert.is.equal(unicode.ascii.match('Test', 'e', 3), nil)
 
-  assert.is.equal(unicode.ascii.match('Test', 'e'), 'e')
-  assert.is.equal(unicode.latin1.match('Test', 'e'), 'e')
-  assert.is.equal(unicode.grapheme.match('Test', 'e'), 'e')
-  assert.is.equal(unicode.utf8.match('Test', 'e'), 'e')
+    assert.is.equal(unicode.ascii.match('Test', 'e'), 'e')
+    assert.is.equal(unicode.latin1.match('Test', 'e'), 'e')
+    assert.is.equal(unicode.grapheme.match('Test', 'e'), 'e')
+    assert.is.equal(unicode.utf8.match('Test', 'e'), 'e')
 end)
 
--- ---
--- ---Returns a string that is the concatenation of `n` copies of the string `s`.
--- ---
--- ---@param s string
--- ---@param n integer
--- ---
--- ---@return string
--- function unicode.utf8.rep(s, n) end
-
 it('unicode.xxx.rep', function()
-  assert.is.equal(unicode.ascii.rep('a', 3), 'aaa')
-  assert.is.equal(unicode.latin1.rep('a', 3), 'aaa')
-  assert.is.equal(unicode.grapheme.rep('a', 3), 'aaa')
-  assert.is.equal(unicode.utf8.rep('a', 3), 'aaa')
+    assert.is.equal(unicode.ascii.rep('a', 3), 'aaa')
+    assert.is.equal(unicode.latin1.rep('a', 3), 'aaa')
+    assert.is.equal(unicode.grapheme.rep('a', 3), 'aaa')
+    assert.is.equal(unicode.utf8.rep('a', 3), 'aaa')
 end)
 
 -- ---
@@ -133,12 +115,12 @@ end)
 -- function unicode.utf8.reverse(s) end
 
 it('unicode.xxx.sub', function()
-  assert.is.equal(unicode.ascii.sub('Test', 2), 'est')
+    assert.is.equal(unicode.ascii.sub('Test', 2), 'est')
 
-  assert.is.equal(unicode.ascii.sub('Test', 2, 3), 'es')
-  assert.is.equal(unicode.latin1.sub('Test', 2), 'est')
-  assert.is.equal(unicode.grapheme.sub('Test', 2), 'est')
-  assert.is.equal(unicode.utf8.sub('Test', 2), 'est')
+    assert.is.equal(unicode.ascii.sub('Test', 2, 3), 'es')
+    assert.is.equal(unicode.latin1.sub('Test', 2), 'est')
+    assert.is.equal(unicode.grapheme.sub('Test', 2), 'est')
+    assert.is.equal(unicode.utf8.sub('Test', 2), 'est')
 end)
 
 -- ---
