e9f2973c480c945e974dcaf6a0cc45b4ded9b8bf
Josef Friedrich <josef@friedrich.rocks>
2023-03-27 22:27:40 +0200

    Document the status lib

--- library/luatex/status.lua
+++ library/luatex/status.lua
@@ -6,6 +6,8 @@
 status = {}
 
 ---
+---* Corresponding C source code: [lstatslib.c#L289-L389](https://github.com/TeX-Live/luatex/blob/f52b099f3e01d53dc03b315e1909245c3d5418d3/source/texk/web2c/luatexdir/lua/lstatslib.c#L289-L389)
+---
 ---@class StatusInfo
 ---@field banner string # terminal display banner, for example `This is LuaHBTeX, Version 1.15.0 (TeX Live 2022)`.
 ---@field best_page_break Node # the current best break (a node)
@@ -107,14 +109,22 @@ status = {}
 ---}
 ---```
 ---
+---* Corresponding C source code: [lstatslib.c#L476-L488](https://github.com/TeX-Live/luatex/blob/f52b099f3e01d53dc03b315e1909245c3d5418d3/source/texk/web2c/luatexdir/lua/lstatslib.c#L476-L488)
+---
 ---@return StatusInfo
 function status.list() end
 
 ---
----The error and warning messages can be wiped with the `resetmessages`
----function.
+---Wipe error and warning messages.
+---
+---* Corresponding C source code: [lstatslib.c#L490-L501](https://github.com/TeX-Live/luatex/blob/f52b099f3e01d53dc03b315e1909245c3d5418d3/source/texk/web2c/luatexdir/lua/lstatslib.c#L490-L501)
+---
 function status.resetmessages() end
 
 ---
----A return value can be set with `setexitcode`.
-function status.setexitcode() end
+---Set the exit code.
+---
+---* Corresponding C source code: [lstatslib.c#L503-L506](https://github.com/TeX-Live/luatex/blob/f52b099f3e01d53dc03b315e1909245c3d5418d3/source/texk/web2c/luatexdir/lua/lstatslib.c#L503-L506)
+---
+---@param code integer
+function status.setexitcode(code) end
