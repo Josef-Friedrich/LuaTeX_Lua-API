a49d920c8909246c231acc82e5cd9019e5ce8e70
Josef Friedrich <josef@friedrich.rocks>
2023-04-01 21:48:34 +0200

    PDF scanner

--- library/luatex/pdfscanner.lua
+++ library/luatex/pdfscanner.lua
@@ -3,15 +3,31 @@
 pdfscanner = {}
 
 ---
-------------------------------------------------------------------------
----Undocumented functions listed in alphabetical order
+---* Corresponding C source code: [lpdfscannerlib.c#L680-L828](https://github.com/TeX-Live/luatex/blob/f52b099f3e01d53dc03b315e1909245c3d5418d3/source/texk/web2c/luatexdir/lua/lpdfscannerlib.c#L680-L828)
 ---
----Document them by sliding them up and place them in the order of the
----official documentation
-------------------------------------------------------------------------
+---
+---@param pdf string|PdfeStream|PdfeArray
+---@param operatortable Operatorable
+---@param info table
+function pdfscanner.scan(pdf, operatortable, info) end
 
----
----Warning! Undocumented code!<p>
----TODO: Please contribute
----https://github.com/Josef-Friedrich/LuaTeX_Lua-API#how-to-contribute
-function pdfscanner.scan() end
+---@class Scanner
+local Scanner = {}
+
+function Scanner:pop () end
+---return only the value of a real or integer
+function Scanner:popnumber () end
+---return only the value of a name
+function Scanner:popname () end
+---return only the value of a string
+function Scanner:popstring () end
+---return only the value of a array
+function Scanner:poparray () end
+---return only the value of a dict
+function Scanner:popdictionary () end
+---return only the value of a boolean
+function Scanner:popboolean () end
+---abort further processing of this scan() call
+function Scanner:done () end
+
+---@alias Operatorable table<string, fun(scanner: Scanner, info: table)>
