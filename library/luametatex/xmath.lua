5858e5a0936b94a8b3da501ef509d529eb51d634
Josef Friedrich <josef@friedrich.rocks>
2023-04-12 15:58:19 +0200

    Document the xmath lib from luametatex

---
+++ library/luametatex/xmath.lua
@@ -0,0 +1,630 @@
+---@meta
+xmath = {}
+
+---
+---Compute the principal value of the arc cosine of `a`.
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L26-L30](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L26-L30)
+---* cppreference.com: [numeric/math/acos](https://en.cppreference.com/w/c/numeric/math/acos)
+---
+---@param a number
+---
+---@return number
+function xmath.acos(a) end
+
+---
+---Compute the inverse hyperbolic cosine of `a`.
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L32-L36](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L32-L36)
+---* cppreference.com: [numeric/math/acosh](https://en.cppreference.com/w/c/numeric/math/acosh)
+---
+---@param a number
+---
+---@return number
+function xmath.acosh(a) end
+
+---
+---Compute the principal values of the arc sine of `a`.
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L38-L42](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L38-L42)
+---* cppreference.com: [numeric/math/asin](https://en.cppreference.com/w/c/numeric/math/asin)
+---
+---@param a number
+---
+---@return number
+function xmath.asin(a) end
+
+---
+---Compute the inverse hyperbolic sine of `a`.
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L44-L48](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L44-L48)
+---* cppreference.com: [numeric/math/asinh](https://en.cppreference.com/w/c/numeric/math/asinh)
+---
+---@param a number
+---
+---@return number
+function xmath.asinh(a) end
+
+---
+---Compute the principal value of the arc tangent of `a`.
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L50-L58](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L50-L58)
+---* cppreference.com: [numeric/math/atan](https://en.cppreference.com/w/c/numeric/math/atan)
+---
+---@param a number
+---
+---@return number
+function xmath.atan(a, b) end
+
+---
+---Compute the arc tangent of `y / x` using the signs of arguments to determine the correct quadrant.
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L60-L64](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L60-L64)
+---* cppreference.com: [numeric/math/atan2](https://en.cppreference.com/w/c/numeric/math/atan2)
+---
+---@param a number
+---
+---@return number
+function xmath.atan2(a, b) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L66-L70](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L66-L70)
+---
+---@param a number
+---
+---@return number
+function xmath.atanh(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L72-L76](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L72-L76)
+---
+---@param a number
+---
+---@return number
+function xmath.cbrt(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L78-L82](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L78-L82)
+---
+---@param a number
+---
+---@return number
+function xmath.ceil(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L84-L88](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L84-L88)
+---
+---@param a number
+---
+---@return number
+function xmath.copysign(a, b) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L90-L94](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L90-L94)
+---
+---@param a number
+---
+---@return number
+function xmath.cos(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L96-L100](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L96-L100)
+---
+---@param a number
+---
+---@return number
+function xmath.cosh(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L102-L106](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L102-L106)
+---
+---@param a number
+---
+---@return number
+function xmath.deg(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L108-L112](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L108-L112)
+---
+---@param a number
+---
+---@return number
+function xmath.erf(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L114-L118](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L114-L118)
+---
+---@param a number
+---
+---@return number
+function xmath.erfc(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L120-L124](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L120-L124)
+---
+---@param a number
+---
+---@return number
+function xmath.exp(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L126-L130](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L126-L130)
+---
+---@param a number
+---
+---@return number
+function xmath.exp2(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L132-L136](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L132-L136)
+---
+---@param a number
+---
+---@return number
+function xmath.expm1(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L138-L142](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L138-L142)
+---
+---@param a number
+---
+---@return number
+function xmath.fabs(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L144-L148](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L144-L148)
+---
+---@param a number
+---
+---@return number
+function xmath.fdim(a, b) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L150-L154](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L150-L154)
+---
+---@param a number
+---
+---@return number
+function xmath.floor(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L156-L160](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L156-L160)
+---
+---@param a number
+---
+---@return number
+function xmath.fma(a, b, c) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L162-L171](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L162-L171)
+---
+---@param ... number
+---
+---@return number
+function xmath.fmax(...) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L173-L182](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L173-L182)
+---
+---@param ... number
+---
+---@return number
+function xmath.fmin(...) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L184-L188](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L184-L188)
+---
+---@param a number
+---
+---@return number
+function xmath.fmod(a, b) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L190-L196](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L190-L196)
+---
+---@param a number
+---
+---@return number
+function xmath.frexp(a, b) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L205-L209](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L205-L209)
+---
+---@param a number
+---
+---@return number
+function xmath.gamma(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L211-L215](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L211-L215)
+---
+---@param a number
+---
+---@return number
+function xmath.hypot(a, b) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L217-L221](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L217-L221)
+---
+---@param a number
+---
+---@return number
+function xmath.isfinite(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L223-L227](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L223-L227)
+---
+---@param a number
+---
+---@return number
+function xmath.isinf(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L229-L233](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L229-L233)
+---
+---@param a number
+---
+---@return number
+function xmath.isnan(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L235-L239](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L235-L239)
+---
+---@param a number
+---
+---@return number
+function xmath.isnormal(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L241-L245](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L241-L245)
+---
+---@param a number
+---
+---@return number
+function xmath.j0(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L247-L251](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L247-L251)
+---
+---@param a number
+---
+---@return number
+function xmath.j1(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L253-L257](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L253-L257)
+---
+---@param a number
+---
+---@return number
+function xmath.jn(a, b) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L259-L263](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L259-L263)
+---
+---@param a number
+---
+---@return number
+function xmath.ldexp(a, b) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L265-L269](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L265-L269)
+---
+---@param a number
+---
+---@return number
+function xmath.lgamma(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L271-L287](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L271-L287)
+---
+---@param a number
+---
+---@return number
+function xmath.log(a, b) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L289-L293](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L289-L293)
+---
+---@param a number
+---
+---@return number
+function xmath.log10(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L295-L299](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L295-L299)
+---
+---@param a number
+---
+---@return number
+function xmath.log1p(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L301-L305](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L301-L305)
+---
+---@param a number
+---
+---@return number
+function xmath.log2(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L307-L311](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L307-L311)
+---
+---@param a number
+---
+---@return number
+function xmath.logb(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L313-L320](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L313-L320)
+---
+---@param a number
+---
+---@return number
+function xmath.modf(a, b) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L322-L326](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L322-L326)
+---
+---@param a number
+---
+---@return number
+function xmath.nearbyint(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L328-L332](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L328-L332)
+---
+---@param a number
+---
+---@return number
+function xmath.nextafter(a, b) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L334-L338](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L334-L338)
+---
+---@param a number
+---
+---@return number
+function xmath.pow(a, b) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L340-L344](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L340-L344)
+---
+---@param a number
+---
+---@return number
+function xmath.rad(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L346-L350](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L346-L350)
+---
+---@param a number
+---
+---@return number
+function xmath.remainder(a, b) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L197-L203](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L197-L203)
+---
+---@param a number
+---
+---@return number
+function xmath.remquo(a, b) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L352-L356](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L352-L356)
+---
+---@param a number
+---
+---@return number
+function xmath.round(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L358-L362](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L358-L362)
+---
+---@param a number
+---
+---@return number
+function xmath.scalbn(a, b) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L364-L368](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L364-L368)
+---
+---@param a number
+---
+---@return number
+function xmath.sin(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L370-L374](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L370-L374)
+---
+---@param a number
+---
+---@return number
+function xmath.sinh(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L376-L380](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L376-L380)
+---
+---@param a number
+---
+---@return number
+function xmath.sqrt(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L382-L386](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L382-L386)
+---
+---@param a number
+---
+---@return number
+function xmath.tan(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L388-L392](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L388-L392)
+---
+---@param a number
+---
+---@return number
+function xmath.tanh(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L394-L398](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L394-L398)
+---
+---@param a number
+---
+---@return number
+function xmath.tgamma(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L400-L404](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L400-L404)
+---
+---@param a number
+---
+---@return number
+function xmath.trunc(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L406-L410](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L406-L410)
+---
+---@param a number
+---
+---@return number
+function xmath.y0(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L412-L416](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L412-L416)
+---
+---@param a number
+---
+---@return number
+function xmath.y1(a) end
+
+---
+---__Reference:__
+---
+---* Corresponding C source code: [lmtxmathlib.c#L418-L422](https://github.com/contextgarden/luametatex/blob/812e28feca3cf5de5f41a7fb3ebf25e4a4b2ae00/source/luarest/lmtxmathlib.c#L418-L422)
+---
+---@param a number
+---
+---@return number
+function xmath.yn(a) end
