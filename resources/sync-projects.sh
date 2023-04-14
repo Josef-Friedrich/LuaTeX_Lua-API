d913e950ea8c7ccc0d1ba4edc43a8d46f53580db
Josef Friedrich <josef@friedrich.rocks>
2023-04-14 08:43:25 +0200

    Add sync script

---
+++ resources/sync-projects.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+
+LUA_HOME="$HOME/repos/lua"
+
+PROJECT_DIR="$LUA_HOME/luatex_api_all"
+
+HASH="$(git rev-parse HEAD)"
+
+COMMIT_MESSAGE="Sync with https://github.com/Josef-Friedrich/LuaTeX_Lua-API/commit/$HASH"
+
+_sync() {
+  local NAME="$1"
+  rsync -av --delete "${PROJECT_DIR}/dist/${NAME}/" "$LUA_HOME/${NAME}/library/"
+  cd "$LUA_HOME/${NAME}"
+
+  git add -Av
+  git commit -m "$COMMIT_MESSAGE"
+  git push -u origin main
+}
+
+rsync -av --delete "${PROJECT_DIR}/library/" "${PROJECT_DIR}/dist/"
+"${PROJECT_DIR}/resources/remove-navigation-table.py"
+
+_sync lualatex
+_sync lualibs
+_sync luametatex
+_sync luatex
