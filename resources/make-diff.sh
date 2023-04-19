af252cb011915357bfd08094f6520433a77a8db0
Josef Friedrich <josef@friedrich.rocks>
2023-04-19 21:58:53 +0200

    Extend diffs

--- resources/make-diff.sh
+++ resources/make-diff.sh
@@ -5,27 +5,28 @@ PROJECT_DIR="${LUA_HOME}/luatex_api_all"
 LIBRARY="${PROJECT_DIR}/library"
 
 _make_diff() {
-  local PROJECT="$1"
-  local SRC="$2"
-  local DEST="$3"
+  local ENGINE="$1"
+  local PROJECT="$2"
+  local SRC="$3"
+  local DEST="$4"
 
   wget \
-    -O "${PROJECT_DIR}/library/luatex/${DEST}.lua_upstream.lua" \
+    -O "${PROJECT_DIR}/library/${ENGINE}/${DEST}.lua_upstream.lua" \
     "https://raw.githubusercontent.com/LuaCATS/${PROJECT}/main/library/${SRC}.lua"
 
   diff -Naur \
-    "${LIBRARY}/luatex/${DEST}.lua_upstream.lua" \
-    "${LIBRARY}/luatex/${DEST}.lua" > \
-    "${PROJECT_DIR}/resources/patches/${DEST}_new.diff"
+    "${LIBRARY}/${ENGINE}/${DEST}.lua_upstream.lua" \
+    "${LIBRARY}/${ENGINE}/${DEST}.lua" > \
+    "${PROJECT_DIR}/resources/patches/${ENGINE}/${DEST}_new.diff"
 }
 
-_make_diff lpeg lpeg lpeg
-_make_diff luafilesystem lfs lfs
-_make_diff lzlib lzlib zlib
-_make_diff md5 md5 md5
-_make_diff luaharfbuzz luaharfbuzz luaharfbuzz
-_make_diff luasocket mbox mbox
-_make_diff luasocket mime mime
-_make_diff luasocket socket socket
-_make_diff slnunicode unicode unicode
-_make_diff luazip zip zip
+_make_diff luatex lpeg          lpeg        lpeg
+_make_diff luatex luafilesystem lfs         lfs
+_make_diff luatex lzlib         lzlib       zlib
+_make_diff luatex md5           md5         md5
+_make_diff luatex luaharfbuzz   luaharfbuzz luaharfbuzz
+_make_diff luatex luasocket     mbox        mbox
+_make_diff luatex luasocket     mime        mime
+_make_diff luatex luasocket     socket      socket
+_make_diff luatex slnunicode    unicode     unicode
+_make_diff luatex luazip        zip         zip
--- resources/patch.sh
+++ resources/patch.sh
@@ -5,24 +5,25 @@ PROJECT_DIR="${LUA_HOME}/luatex_api_all"
 LIBRARY="${PROJECT_DIR}/library"
 
 _patch() {
-  local PROJECT="$1"
-  local FILENAME="$2"
+  local ENGINE="$1"
+  local PROJECT="$2"
+  local FILENAME="$3"
   wget \
-    -O "${PROJECT_DIR}/library/luatex/${FILENAME}.lua" \
+    -O "${PROJECT_DIR}/library/${ENGINE}/${FILENAME}.lua" \
     "https://raw.githubusercontent.com/LuaCATS/${PROJECT}/main/library/${FILENAME}.lua"
 
   patch \
-    "${PROJECT_DIR}/library/luatex/${FILENAME}.lua" < \
-    "${PROJECT_DIR}/resources/patches/${FILENAME}.diff"
+    "${PROJECT_DIR}/library/${ENGINE}/${FILENAME}.lua" < \
+    "${PROJECT_DIR}/resources/patches/${ENGINE}/${FILENAME}.diff"
 }
 
-_patch lpeg          lpeg
-_patch luafilesystem lfs
-_patch lzlib         zlib
-_patch md5           md5
-_patch luaharfbuzz   luaharfbuzz
-_patch luasocket     mbox
-_patch luasocket     mime
-_patch luasocket     socket
-_patch slnunicode    unicode
-_patch luazip        zip
+_patch luatex lpeg          lpeg
+_patch luatex luafilesystem lfs
+_patch luatex lzlib         zlib
+_patch luatex md5           md5
+_patch luatex luaharfbuzz   luaharfbuzz
+_patch luatex luasocket     mbox
+_patch luatex luasocket     mime
+_patch luatex luasocket     socket
+_patch luatex slnunicode    unicode
+_patch luatex luazip        zip
