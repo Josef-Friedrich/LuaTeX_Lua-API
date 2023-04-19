e8a062d822b3b94adbc95ef5f298aa34988fee84
Josef Friedrich <josef@friedrich.rocks>
2023-04-19 22:07:58 +0200

    Update diff

--- resources/make-diff.sh
+++ resources/make-diff.sh
@@ -7,26 +7,25 @@ LIBRARY="${PROJECT_DIR}/library"
 _make_diff() {
   local ENGINE="$1"
   local PROJECT="$2"
-  local SRC="$3"
-  local DEST="$4"
+  local FILENAME="$3"
 
   wget \
-    -O "${PROJECT_DIR}/library/${ENGINE}/${DEST}.lua_upstream.lua" \
-    "https://raw.githubusercontent.com/LuaCATS/${PROJECT}/main/library/${SRC}.lua"
+    -O "${PROJECT_DIR}/library/${ENGINE}/${FILENAME}.lua_upstream.lua" \
+    "https://raw.githubusercontent.com/LuaCATS/${PROJECT}/main/library/${FILENAME}.lua"
 
   diff -Naur \
-    "${LIBRARY}/${ENGINE}/${DEST}.lua_upstream.lua" \
-    "${LIBRARY}/${ENGINE}/${DEST}.lua" > \
-    "${PROJECT_DIR}/resources/patches/${ENGINE}/${DEST}_new.diff"
+    "${LIBRARY}/${ENGINE}/${FILENAME}.lua_upstream.lua" \
+    "${LIBRARY}/${ENGINE}/${FILENAME}.lua" > \
+    "${PROJECT_DIR}/resources/patches/${ENGINE}/${FILENAME}_new.diff"
 }
 
-_make_diff luatex lpeg          lpeg        lpeg
-_make_diff luatex luafilesystem lfs         lfs
-_make_diff luatex lzlib         zlib        zlib
-_make_diff luatex md5           md5         md5
-_make_diff luatex luaharfbuzz   luaharfbuzz luaharfbuzz
-_make_diff luatex luasocket     mbox        mbox
-_make_diff luatex luasocket     mime        mime
-_make_diff luatex luasocket     socket      socket
-_make_diff luatex slnunicode    unicode     unicode
-_make_diff luatex luazip        zip         zip
+_make_diff luatex lpeg          lpeg
+_make_diff luatex luafilesystem lfs
+_make_diff luatex lzlib         zlib
+_make_diff luatex md5           md5
+_make_diff luatex luaharfbuzz   luaharfbuzz
+_make_diff luatex luasocket     mbox
+_make_diff luatex luasocket     mime
+_make_diff luatex luasocket     socket
+_make_diff luatex slnunicode    unicode
+_make_diff luatex luazip        zip
