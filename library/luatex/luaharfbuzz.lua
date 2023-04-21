7f0591b6ab9d9c43d68252281d5db8d6025f9892
Josef Friedrich <josef@friedrich.rocks>
2023-04-21 17:20:30 +0200

    Fix examples

--- library/luatex/luaharfbuzz.lua
+++ library/luatex/luaharfbuzz.lua
@@ -322,6 +322,7 @@ function Face:get_table_tags() end
 ---local face = luaharfbuzz.Face.new('/usr/share/fonts/truetype/noto/NotoSans-Regular.ttf')
 ---if face == nil then error("Font not found!") end
 
+---
 ---print('Units per em', face:get_upem()) -- 1000
 ---```
 ---
