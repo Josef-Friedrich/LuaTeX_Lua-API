94c718ee5a5366f7c4ad453c5d3f108ab162bd89
Josef Friedrich <josef@friedrich.rocks>
2025-07-12 15:02:10 +0200

    fix navigation removal

--- resources/remove-navigation-table.py
+++ resources/remove-navigation-table.py
@@ -13,7 +13,7 @@ def patch_file(file_name: str):
         content = src.read()
 
     # Remove the navigation table
-    content = re.sub(r"^_N\..+(?=\n)", "", content, re.MULTILINE)
+    content = re.sub(r"^_N\..+(?=\n)", "", content, flags=re.MULTILINE)
 
     # Remove duplicate empty lines.
     content = re.sub("\n\n+", "\n\n", content)
