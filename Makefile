b8b329454da914f261af57ac501dad0f3c9af64f
Josef Friedrich <josef@friedrich.rocks>
2025-07-19 22:30:09 +0200

    Merge some Python scripts

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
-all: fix_lua_docstrings convert_tex_to_lua stylua
+all: format stylua
 
-fix_lua_docstrings:
-	resources/fix-lua-docstrings.py
+format:
+	./manage.py format
 
 convert_tex_to_lua:
 	resources/convert-tex-to-lua-docstrings.py
@@ -26,7 +26,7 @@ dist: fix_lua_docstrings clean
 
 dist_rsync:
 	rsync -av --delete ./library/ ./dist/
-	./resources/remove-navigation-table.py
+	./manage.py remove-navigation-table
 
 update_lls_addons:
 	resources/update-lls-addons.sh
@@ -50,8 +50,6 @@ ctan_luatex: dist_rsync
 	tar cvfz luatex-type-definitions.tar.gz luatex-type-definitions
 	rm -rf luatex-type-definitions
 
-
-
 clean:
 	git clean -dfX
 
@@ -59,4 +57,4 @@ update_manual:
 	wget -O /usr/local/texlive/texmf-dist/doc/luatex/base/luatex.pdf https://gitlab.lisn.upsaclay.fr/texlive/luatex/-/raw/master/manual/luatex.pdf
 	wget -O /usr/local/texlive/texmf-dist/doc/context/documents/general/manuals/luametatex.pdf https://raw.githubusercontent.com/contextgarden/context/main/doc/context/documents/general/manuals/luametatex.pdf
 
-.PHONY: all fix_lua_docstrings convert_tex_to_lua stylua debug print_namespace generate_doc dist update_lls_addons test diff patch clean update_manual
+.PHONY: all format convert_tex_to_lua stylua debug print_namespace generate_doc dist update_lls_addons test diff patch clean update_manual
--- manage.py
+++ manage.py
@@ -1,20 +1,27 @@
 #! /usr/bin/python
 
 import argparse
+import sys
 import tempfile
 from pathlib import Path
 import subprocess
 import re
 import glob
-from typing import Literal
+from typing import Callable, Literal
 
 project_base_path: Path = Path(__file__).resolve().parent
 
 
-def open_file(path: Path) -> None:
+def _open_file(path: Path) -> None:
     subprocess.call(
         ["xdg-open", str(path)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
     )
+
+
+def _apply_function_on_glob(glob_relpath: str, fn: Callable[[str], None]) -> None:
+    for file_name in glob.glob(str(project_base_path) + "/" + glob_relpath):
+        print(file_name)
+        fn(file_name)
 
 
 Subproject = Literal[
@@ -74,13 +81,40 @@ def compile_example(src_relpath: str) -> None:
     pdf: Path = dest.with_suffix(".pdf")
 
     if pdf.exists():
-        open_file(pdf)
+        _open_file(pdf)
+
+
+def format_docstrings() -> None:
+    def _format(file_name: str) -> None:
+        content: str = ""
+
+        with open(file_name) as src:
+            content = src.read()
+
+        # Start a docstring with an empty comment line.
+        content = re.sub(r"\n\n---(?=[^\n])", r"\n\n---\n---", content)
+
+        # Remove duplicate empty comment lines.
+        content = re.sub("\n---(\n---)+\n", "\n---\n", content)
+
+        # Side effect with code examples in Lua docstrings
+        # content = content.replace(") end\n---", ") end\n\n---")
+
+        # Add an empty comment line before the @param annotation.
+        # content = re.sub(
+        #     r"(?<!\n---)\n---@param(?=.*?\n.*?@param)", r"\n---\n---@param", content
+        # )
+
+        with open(file_name, "w") as dest:
+            dest.write(content)
+
+    _apply_function_on_glob("library/**/*.lua", _format)
 
 
 def merge_type_definitions(subproject: Subproject = "luatex") -> None:
     contents: list[str] = []
 
-    def merge_file(file_name: str) -> None:
+    def _merge(file_name: str) -> None:
         content: str = ""
         with open(file_name) as src:
             content = src.read()
@@ -88,16 +122,40 @@ def merge_type_definitions(subproject: Subproject = "luatex") -> None:
         content = re.sub(r"^return .+\n", "", content, flags=re.MULTILINE)
         contents.append(content)
 
-    for file_name in sorted(
-        glob.glob(str(project_base_path) + "/dist/" + subproject + "/*.lua")
-    ):
-        print(file_name)
-        merge_file(file_name)
+    _apply_function_on_glob("dist/" + subproject + "/*.lua", _merge)
 
     dist_dir: Path = project_base_path / "dist-ctan" / subproject
     dist_dir.mkdir(parents=True, exist_ok=True)
     with open(dist_dir / (subproject + "-type-definitions.lua"), "w") as f:
         f.write("\n".join(contents))
+
+
+def remove_navigation_table() -> None:
+    def _remove(file_name: str) -> None:
+        content: str = ""
+
+        with open(file_name) as src:
+            content = src.read()
+
+        content = content.replace(
+            "---A helper table to better navigate through the documentation using the\n"
+            + "---outline: https://github.com/Josef-Friedrich/LuaTeX_Lua-API#navigation-table-_n\n",
+            "",
+        )
+
+        # Remove the navigation table
+        content = re.sub(r"^_N.+\n", "", content, flags=re.MULTILINE)
+
+        # Remove duplicate empty lines.
+        content = re.sub("\n\n+", "\n\n", content)
+
+        # Remove leading and trailing whitespace
+        content = content.strip() + "\n"
+
+        with open(file_name, "w") as dest:
+            dest.write(content)
+
+    _apply_function_on_glob("dist/**/*.lua", _remove)
 
 
 if __name__ == "__main__":
@@ -110,6 +168,11 @@ if __name__ == "__main__":
     )
     example_parser.add_argument("relpath")
 
+    format_parser = subparsers.add_parser(
+        "format",
+        help="Format the lua docstrings (Remove duplicate empty comment lines, start docstring with an empty line)",
+    )
+
     merge_parser = subparsers.add_parser(
         "merge",
         help="Merge multiple definitions files into one for the CTAN upload.",
@@ -117,11 +180,21 @@ if __name__ == "__main__":
     )
     merge_parser.add_argument("subproject")
 
+    navigation_parser = subparsers.add_parser(
+        "remove-navigation-table",
+        help="Remove the navigation table in all subprojects in the dist folder.",
+    )
+
     args = parser.parse_args()
 
     if args.command == "example":
         compile_example(args.relpath)
+    elif args.command == "format":
+        format_docstrings()
     elif args.command == "merge":
         merge_type_definitions(args.subproject)
+    elif args.command == "remove-navigation-table":
+        remove_navigation_table()
     else:
         parser.print_help()
+        sys.exit(1)
