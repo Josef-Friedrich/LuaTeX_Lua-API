5eb42313519e4a4f1e695dea86aa348180eae240
Josef Friedrich <josef@friedrich.rocks>
2025-07-19 20:46:46 +0200

    Unify some python script in one management script

--- resources/management-tool.py
+++ resources/management-tool.py
@@ -4,7 +4,9 @@ import argparse
 import tempfile
 from pathlib import Path
 import subprocess
-
+import re
+import glob
+from typing import Literal
 
 project_base_path: Path = Path(__file__).resolve().parent.parent
 
@@ -15,11 +17,39 @@ def open_file(path: Path) -> None:
     )
 
 
-def compile_example(src_relpath: str) -> None:
-    tmp_dir = Path(tempfile.mkdtemp())
-    print(f"Temp directory: {tmp_dir}")
+Subproject = Literal[
+    "lualatex",
+    "lualibs",
+    "luametatex",
+    "luaotfload",
+    "luatex",
+]
 
-    src: Path = project_base_path / "examples" / src_relpath
+
+def compile_example(src_relpath: str) -> None:
+    """
+    Compiles a Lua or TeX example file using LuaTeX.
+
+    Given a relative path to a source file (either Lua or TeX), this function:
+    - Copies the source file to a temporary directory.
+    - If the source is a Lua file, generates a TeX wrapper that runs the Lua file via \\directlua.
+    - Runs LuaTeX on the TeX file in the temporary directory.
+    - Prints the output from LuaTeX.
+    - If a PDF is generated, opens the PDF file.
+
+    Args:
+        src_relpath (str): Relative path to the source example file (Lua or TeX).
+
+    Returns:
+        None
+    """
+    tmp_dir = Path(tempfile.mkdtemp())
+
+    src: Path
+    if src_relpath.startswith("examples"):
+        src = project_base_path / src_relpath
+    else:
+        src = project_base_path / "examples" / src_relpath
 
     dest: Path = tmp_dir / src.name
     dest.write_bytes(src.read_bytes())
@@ -29,10 +59,8 @@ def compile_example(src_relpath: str) -> None:
 
     if src.suffix == ".lua":
         dest_lua = dest
-        print(dest_lua.read_text())
         dest_tex = dest.with_suffix(".tex")
         dest_tex.write_text("\\directlua{dofile('" + str(dest_lua) + "')}\n\\bye\n")
-        print(dest_tex.read_text())
     else:
         dest_tex = dest
         dest_lua = dest.with_suffix(".lua")
@@ -41,22 +69,59 @@ def compile_example(src_relpath: str) -> None:
         ["luatex", str(dest_tex)], capture_output=True, text=True, cwd=tmp_dir
     )
 
-    # print("Luatex stdout:")
     print(result.stdout)
-    # print("Luatex stderr:")
-    # print(result.stderr)
 
-    pdf = dest.with_suffix(".pdf")
-    open_file(pdf)
+    pdf: Path = dest.with_suffix(".pdf")
+
+    if pdf.exists():
+        open_file(pdf)
+
+
+def merge_type_definitions(subproject: Subproject = "luatex") -> None:
+    contents: list[str] = []
+
+    def merge_file(file_name: str) -> None:
+        content: str = ""
+        with open(file_name) as src:
+            content = src.read()
+        # Remove the return statements
+        content = re.sub(r"^return .+\n", "", content, flags=re.MULTILINE)
+        contents.append(content)
+
+    for file_name in sorted(
+        glob.glob(str(project_base_path) + "/dist/" + subproject + "/*.lua")
+    ):
+        print(file_name)
+        merge_file(file_name)
+
+    dist_dir: Path = project_base_path / "dist-ctan" / subproject
+    dist_dir.mkdir(parents=True, exist_ok=True)
+    with open(dist_dir / (subproject + "-type-definitions.lua"), "w") as f:
+        f.write("\n".join(contents))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest="command")
-    example_parser = subparsers.add_parser("example")
+    example_parser = subparsers.add_parser(
+        "example",
+        help="Compile examples in the folder ./examples",
+        description="You can specify the relative path of a Lua file or of a TeX file.",
+    )
     example_parser.add_argument("relpath")
+
+    merge_parser = subparsers.add_parser(
+        "merge",
+        help="Merge multiple definitions files into one for the CTAN upload.",
+        description="Specify the subproject folder.",
+    )
+    merge_parser.add_argument("subproject")
 
     args = parser.parse_args()
 
     if args.command == "example":
         compile_example(args.relpath)
+    elif args.command == "merge":
+        merge_type_definitions(args.subproject)
+    else:
+        parser.print_help()
