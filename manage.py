a3a6cce6138340581fee5b662c6b17b8eead89b9
Josef Friedrich <josef@friedrich.rocks>
2025-08-06 09:58:27 +0200

    Update submodules

--- manage.py
+++ manage.py
@@ -904,7 +904,7 @@ markdown_extensions:
 
     gh_pages_repo = project_base_path / "subrepos" / "Lua-TeX" / subproject_title
 
-    if gh_pages_repo.exists():
+    if gh_pages_repo.is_dir():
         subprocess.check_call(["git", "branch", "gh-pages"], cwd=gh_pages_repo)
         subprocess.check_call(["git", "checkout", "gh-pages"], cwd=gh_pages_repo)
         subprocess.check_call(["git", "pull"], cwd=gh_pages_repo)
