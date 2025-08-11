e1e95db105467c5d88445589adaa44b655655800
Josef Friedrich <josef@friedrich.rocks>
2025-08-11 16:29:00 +0200

    Update lpeg

--- examples/luatex/lpeg/S.lua
+++ examples/luatex/lpeg/S.lua
@@ -2,8 +2,8 @@
 local assert = require("utils").assert
 
 local pattern = lpeg.S('+-*/')
-
-
 assert.equals(pattern:match("+"), 2)
-assert.equals(pattern:match("++"), 6)
-assert.equals(pattern:match("++"), 6)
+assert.equals(pattern:match("-"), 2)
+assert.equals(pattern:match("*"), 2)
+assert.equals(pattern:match("/"), 2)
+assert.is_nil(pattern:match("x"))
